ce5944474bf01a822221edbcfdb4b3be5ade0fc3
Gpsnm <adam-willetts@hotmail.co.uk>
2022-03-05 18:49:46 +0000

    Added ability to populate screen.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,41 @@
 
 let display = document.getElementById('display');
-let buttons = document.getElementsByClassName('button');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click',(e) => {
+     switch(e.target.innerText){
+        case 'CLEAR':
+            display.innerText = '';
+            break;
+            case 'DELETE':
+                display.innerText = display.innerText.slice(0,-1);
+                break;
+        default:
+            display.innerText += e.target.innerText;
+            
+    
+    }
+    })
+})
 
 
 
 
-function operate(a,b,op){
-switch(op){
-    case '+':
-        return a+b;
-        case '-':
-            return a-b;
-            case '/':
-                return a/b;
-                case '*':
-                    return a*b;
-                    default:
-                        return 'invalid arguement';
-};
-};
+
+
+// function operate(a,b,op){
+// switch(op){
+//     case '+':
+//         return a+b;
+//         case '-':
+//             return a-b;
+//             case '/':
+//                 return a/b;
+//                 case '*':
+//                     return a*b;
+//                     default:
+//                         return 'invalid arguement';
+// };
+// };
 
